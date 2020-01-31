1c79cb044f2239ec5e6ea01811e6e6c82c7a6fad
Theodore Wu <twu00@stuy.edu>
2020-01-31 15:33:02 -0500

    added the file input method

--- picMaker.py
+++ picMaker.py
@@ -3,11 +3,22 @@ class pixel:
         self.red = r
         self.green = g
         self.blue = b
+    def getColor(self):
+        return "{} {} {}".format(self.red, self.green, self.blue)
 class image:
-    def __init__(self, n):
+    def __init__(self, h, w = h):
         self.pixels = [[pixel() for i in range(n)] for i in range[n]]
+        self.height = h
+        self.width = w
     def fileInput(file):
-        return
+        enter = "p3\n {} {} \n".format(self.height, self.width)
+        for i in self.pixels:
+            row = ""
+            for p in i:
+                row += p.getColor()
+            enter += row + "\n"
+        with open(file+".ppm", "r+") as f:
+            f.write(enter)
 
 
     
