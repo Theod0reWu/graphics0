013fb5c7d7db0825936b6963195fd24908428e72
Theodore Wu <twu00@stuy.edu>
2020-01-31 15:25:19 -0500

    started

---
+++ picMaker.py
@@ -0,0 +1,13 @@
+class pixel:
+    def __init__(self, r = 0, g = 0, b = 0):
+        self.red = r
+        self.green = g
+        self.blue = b
+class image:
+    def __init__(self, n):
+        self.pixels = [[pixel() for i in range(n)] for i in range[n]]
+    def fileInput(file):
+        return
+
+
+    
