9c0d08f6fd6358016b7ef4b50c23c284005186dc
Theod0reWu <twu00@stuy.edu>
2020-02-02 23:35:00 -0500

    added pics

--- picMaker.py
+++ picMaker.py
@@ -6,19 +6,56 @@ class pixel:
     def getColor(self):
         return "{} {} {}".format(self.red, self.green, self.blue)
 class image:
-    def __init__(self, h, w = h):
-        self.pixels = [[pixel() for i in range(n)] for i in range[n]]
+    def __init__(self, h, w):
+        self.pixels = [[pixel() for i in range(w)] for i in range(h)]
         self.height = h
         self.width = w
-    def fileInput(file):
-        enter = "p3\n {} {} \n".format(self.height, self.width)
+    def toFile(self,file):
+        enter = "P3\n{} {}\n255\n".format(self.height, self.width)
         for i in self.pixels:
             row = ""
             for p in i:
-                row += p.getColor()
+                row += p.getColor() + " "
             enter += row + "\n"
-        with open(file+".ppm", "r+") as f:
+        with open(file+".ppm", "w+") as f:
             f.write(enter)
-
-
+    def printIt(self):
+        enter = "P3\n{} {}\n255\n".format(self.height, self.width)
+        for i in self.pixels:
+            row = ""
+            for p in i:
+                row += p.getColor() + " "
+            enter += row + "\n"
+        print(enter)
+    def allPix(self, funct): #funct is a function that takes in a pixel and modifies it
+        for r in self.pixels:
+            for c in r:
+                funct(c)
+    def allPixCoord(self, funct):
+        for h in range(self.height):
+            for w in range(self.width):
+                funct(w,h,self.pixels[w][h])
     
+nothing = image(500,500)
+from random import randint
+def rando(pixel):
+    pixel.red = randint(0,255)
+    pixel.green = randint(0,255)
+    pixel.blue = randint(0,255)
+#nothing.allPix(rando)
+#nothing.printIt()
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < 80:
+        z = z*z + c
+        n += 1
+    return n
+
+def man(w,h,pixel):
+    c = complex(-2 + (w / 500) * (1 - -2),-1 + (h / 500) * (1 - -1))
+    m = mandelbrot(c)
+    pixel.red = 255 - int(m * 255 / 80)
+nothing.allPixCoord(man)
+nothing.toFile("image")
+
